5f86505a96fcaf396f414ead80901cb65bba75fc
benaya josua <benaya.josua@gmail.com>
2026-02-03 07:16:29 +0700

    add : tambahin fitur untuk works CRUD

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import dotenv from "dotenv";
 import prisma from "./lib/prisma.js";
 
 import journeyRoute from "./routes/journey.route.js"
-import authRoutes from "./routes/auth.route.js"
+import authRoute from "./routes/auth.route.js"
+import workRoute from "./routes/works.route.js"
 
 dotenv.config();
 
@@ -17,8 +18,9 @@ app.use(express.urlencoded({ extended: true }));
 await prisma.$connect();
 console.log("Prisma connected");
 
-app.use("/auth", authRoutes);
-app.use("/journeys", journeyRoute);
+app.use("/auth", authRoute)
+app.use("/journeys", journeyRoute)
+app.use("/works", workRoute)
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("halobenaya testing API");
