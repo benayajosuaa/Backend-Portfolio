e75df3b93522fc4dacbd5c61911b20aa538d38e4
benaya josua <benaya.josua@gmail.com>
2026-02-04 18:35:38 +0700

    add : tambahin route untuk API contact

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import prisma from "./lib/prisma.js";
 import journeyRoute from "./routes/journey.route.js"
 import authRoute from "./routes/auth.route.js"
 import workRoute from "./routes/works.route.js"
+import contactRoute from "./routes/contact.route.js"
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ console.log("Prisma connected");
 app.use("/auth", authRoute)
 app.use("/journeys", journeyRoute)
 app.use("/works", workRoute)
+app.use("/contact", contactRoute)
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("halobenaya testing API");
